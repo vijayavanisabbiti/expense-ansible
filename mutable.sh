d43188c9a4cc1fafefa318555eda2c3827d2d165
VIJAYAVANI <vijayavanimanju@gmail.com>
2024-01-20 16:22:09 +0530

    mutable file

--- mutable.sh
+++ mutable.sh
@@ -6,7 +6,7 @@
 
 
 # Find the servers (Anisble Dynamic Inventory)
-aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text
+aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 
 # Change the Parameter store having the app version.
 aws ssm put-parameter --name "${ENV}.${COMPONENT}.app_version" --value "${APP_VERSION}" --type "String" --overwrite
