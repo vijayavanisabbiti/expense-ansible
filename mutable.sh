451cef90effe1cdcd7b88ad83e727e81becd4011
VIJAYAVANI <vijayavanimanju@gmail.com>
2024-01-20 17:03:59 +0530

    password

--- mutable.sh
+++ mutable.sh
@@ -10,6 +10,7 @@ aws ec2 describe-instances --filters "Name=tag:Name,Values=${ENV}-${COMPONENT}" 
 
 # Change the Parameter store having the app version.
 aws ssm put-parameter --name "${ENV}.${COMPONENT}.app_version" --value "${APP_VERSION}" --type "String" --overwrite
+
 SSH_PASSWORD=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
 
 #Run ansible playbook push on the server
